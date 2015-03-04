3d43c36406b4d0bcd1ef25df3445843ff2629240
carlos silva <cefs1984@gmail.com>
2015-03-04 01:17:28 -0300

    ListView Gastos da Viagem -Implementado novo resource para cores. -Classe anonima que estende ViewBinder para controlar o SimpleAdapter.

--- src/br/com/casadocodigo/boaviagem/GastoListActivity.java
+++ src/br/com/casadocodigo/boaviagem/GastoListActivity.java
@@ -1,41 +1,88 @@
 package br.com.casadocodigo.boaviagem;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class GastoListActivity extends ListActivity
 								implements OnItemClickListener{
+	
+	private List<Map<String, Object>> gastos;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		setListAdapter(new ArrayAdapter<String>(this, 
-				android.R.layout.simple_list_item_1, listarGastos()));
-		ListView listview = getListView();
-		listview.setOnItemClickListener(this);
+		String[] de = { "data", "descricao", "valor", "categoria"};
+		int[] para = { R.id.data, R.id.descricao,
+					   R.id.valor, R.id.categoria };
+		
+		SimpleAdapter adapter = new SimpleAdapter(this,
+					listarGastos(), R.layout.lista_gasto, de, para);
+		
+		adapter.setViewBinder(new GastoViewBinder());
+		
+		setListAdapter(adapter);
+		getListView().setOnItemClickListener(this);
  	}
 	
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
-		TextView textView = (TextView) view;
-		Toast.makeText(this, "Gasto selecionado" + textView.getText(),
-						Toast.LENGTH_SHORT).show();
+		
+		Map<String, Object> map = gastos.get(position);
+		String descricao = (String) map.get("descricao");
+		String mensagem = "Gasto selecionada: " + descricao; 
+		Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+	}
+		
+	private List<Map<String, Object>> listarGastos() {
+		gastos = new ArrayList<Map<String, Object>>();
+		
+		Map<String, Object> item = new HashMap<String, Object>();
+		item.put("data", "04/02/2012");
+		item.put("descricao", "Diária Hotel");
+		item.put("valor", "R$ 260,00");
+		item.put("categoria", R.color.categoria_hospedagem);
+		gastos.add(item);
+		
+		return gastos;
 	}
 	
-	public List<String> listarGastos() {
-		return Arrays.asList("Sanduíche R$ 19,90",
-						 	 "Táxi Aeroporto - Hotel R$ 34,00",
-						 	 "Revista R$ 12,00");
+	private String dataAnterior = "";
+	private class GastoViewBinder implements ViewBinder {
+		
+		public boolean setViewValue(View view, Object data,
+				String textRepresentation) {
+			if (view.getId() == R.id.data) {
+				if (!dataAnterior.equals(data)) {
+					TextView textView = (TextView) view;
+                    textView.setText(textRepresentation);
+	                    dataAnterior = textRepresentation;
+	                    view.setVisibility(View.VISIBLE);
+					} else {
+						view.setVisibility(View.GONE);
+					}
+					
+					return true;
+				}
+				
+				 if(view.getId() == R.id.categoria){
+	                Integer id = (Integer) data;
+	                view.setBackgroundColor(getResources().getColor(id));
+	                return true;
+	            }
+	            return false;
+		}
 	}
 }
