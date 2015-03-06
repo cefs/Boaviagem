ee26b9bc680a24b217a490412f4f4a79534b55b9
carlos silva <cefs1984@gmail.com>
2015-03-06 00:42:28 -0300

    Preferências do Usuário: -Acessando Prefências do celular para interferir na aplicação.

--- src/br/com/casadocodigo/boaviagem/DashboardActivity.java
+++ src/br/com/casadocodigo/boaviagem/DashboardActivity.java
@@ -14,32 +14,35 @@ public class DashboardActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dashboard);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.dashboard_menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		finish();
 		return true;
 	}
-	
+
 	public void selecionarOpcao(View view) {
 		/*
-	 	com base na view  que foi clicada
-	 	iremos tomar a ação correta
+		 * com base na view que foi clicada iremos tomar a ação correta
 		 */
 		switch (view.getId()) {
 		case R.id.nova_viagem:
-		 	startActivity(new Intent(this, ViagemActivity.class));
-		 	break;
+			startActivity(new Intent(this, ViagemActivity.class));
+			break;
 		case R.id.novo_gasto:
 			startActivity(new Intent(this, GastoActivity.class));
 			break;
+		case R.id.configuracoes:
+			startActivity(new Intent(this, ConfiguracoesActivity.class));
+			break;
 		}
+
 	}
 }
